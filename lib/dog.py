0140288e988eb76af79efc58ca0eb67836d81a18
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-02-16 01:34:04 +0300

    Completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    #a method that takes in an argument of the dogs name
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+fido = Dog("Poseidon")
+print(fido.name)
+print(fido.breed)
